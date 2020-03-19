6cbeb937c1b2c9fbb2eca33e1f0d012f239563f9
Thomas Draier <tdraier@jahia.com>
2020-03-19 21:49:00 +0100

    BACKLOG-12272: Updated lobel/icon/ordering

--- src/javascript/register.js
+++ src/javascript/register.js
@@ -2,7 +2,7 @@
     window.jahia.i18n.loadNamespaces('site-settings-seo');
 
     window.jahia.uiExtender.registry.add('adminRoute', 'site-settings-seo', {
-        targets: ['jcontent'],
+        targets: ['jcontent:30'],
         icon: null,
         label: 'site-settings-seo:label.title',
         isSelectable: true,
