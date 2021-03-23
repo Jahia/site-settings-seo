9b78e865b5bdfec9210ae046ae4152f76f5f5be0
alex <alexandrk_@hotmail.com>
2021-03-23 07:53:50 -0400

    BACKLOG-15718 Remove unused code

--- src/javascript/siteSettingsSeo-app.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {SiteSettingsSeo} from './components/SiteSettingsSeo';
-
-window.reactRender = function (target, id, dxContext) {
-    ReactDOM.render(<SiteSettingsSeo id={id} dxContext={dxContext}/>, document.getElementById(target));
-};
