f0df9863d061e9bde520d92981e82d3edb2198b1
Xiang Yu He <xhe@jahia.com>
2021-06-03 20:26:53 -0400

    Add-in dummy file.

--- tests/cypress/integration/sandboxTest.spec.ts
+++ tests/cypress/integration/sandboxTest.spec.ts
@@ -1,14 +1,8 @@
-import { home } from '../page-object/home.page'
-import { sandboxPage } from '../page-object/sandbox.page'
-
+// Dummy test file
 describe('Successfully navigates to sandbox app', () => {
     it('successfully navigate to the app', () => {
         cy.visit('/', {
             failOnStatusCode: false
         })
-        home.login('root', Cypress.env('SUPER_USER_PASSWORD'), true)
-
-        sandboxPage.goTo()
-        expect(sandboxPage.getByText('button', 'Some Action')).not.to.be.undefined
     })
 })
