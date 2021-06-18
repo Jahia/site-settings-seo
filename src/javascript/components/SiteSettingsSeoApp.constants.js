471f437881af218f5bf6b191cce86e0c59698063
Geofrey Flores <gflores@jahia.com>
2021-06-18 14:45:40 -0400

    QA-13765: Modify vanity URL validation

--- src/javascript/components/SiteSettingsSeoApp.constants.js
+++ src/javascript/components/SiteSettingsSeoApp.constants.js
@@ -1,5 +1,5 @@
 export default {
-    MAPPING_REG_EXP: new RegExp('^/?(?!.*/{2,})[a-zA-Z_0-9\\-\\./]+$'),
+    SITES_REG_EXP: /^\/?sites/,
     NB_NEW_MAPPING_ROWS: 1,
     TABLE_POLLING_INTERVAL: 2000
 };
