b32790f7608814b199bc7015c22501c540d0d7d9
Francois Gerthoffert <f.gerthoffert@gmail.com>
2021-12-07 13:12:49 -0500

    Removed tests

--- tests/cypress/integration/sandboxTest.spec.ts
+++
@@ -1,8 +0,0 @@
-// Dummy test file
-describe('Successfully navigates to sandbox app', () => {
-    it('successfully navigate to the app', () => {
-        cy.visit('/', {
-            failOnStatusCode: false
-        })
-    })
-})
