8f623db725a29ad08e0345f3bedc5e5c0441e8dc
jsinovassin <jsinovassinnaik@jahia.com>
2023-02-03 12:05:01 +0100

    BACKLOG-20606 : remove useless line in statuses

--- core/src/javascript/components/VanityList/DefaultRow.jsx
+++ core/src/javascript/components/VanityList/DefaultRow.jsx
@@ -38,8 +38,7 @@ export const DefaultRow = ({
         const statuses = [
             {key: 'PUBLISHED', value: classes.isPublished},
             {key: 'MODIFIED', value: classes.toBePublished},
-            {key: 'NOT_PUBLISHED', value: classes.notPublished},
-            {key: 'MARKED_FOR_DELETION', value: classes.isMarkedForDeletion}
+            {key: 'NOT_PUBLISHED', value: classes.notPublished}
         ];
 
         const isMarkedForDeletion = url.publicationInfo.publicationStatus === 'MARKED_FOR_DELETION' || url.mixinTypes.find(mixin => mixin.name === 'jmix:markedForDeletion');
