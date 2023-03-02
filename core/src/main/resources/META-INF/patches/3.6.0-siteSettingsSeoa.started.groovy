ee74e475e3be1013aef5fc78a6e8a2ff50d83aec
jahia-carp <46920876+jahia-carp@users.noreply.github.com>
2023-03-02 16:51:39 +0100

    BACKLOG-20705: Update migration script to take care about inconsistent system name (#132)

--- core/src/main/resources/META-INF/patches/3.6.0-siteSettingsSeoa.started.groovy
+++ core/src/main/resources/META-INF/patches/3.6.0-siteSettingsSeoa.started.groovy
@@ -1,4 +1,5 @@
-import org.jahia.api.Constants;
+import org.jahia.api.Constants
+import org.jahia.services.content.JCRContentUtils;
 import org.jahia.services.content.JCRNodeWrapper;
 import org.jahia.services.content.JCRSessionWrapper;
 import org.jahia.services.content.JCRTemplate;
@@ -12,7 +13,33 @@ import java.util.LinkedList;
 import java.util.List;
 
 private void restoreDeletedVanitys() throws RepositoryException {
-    copyMissingVanitysFromLiveToDefault(findAllVanitysInLive());
+    copyMissingVanitysFromLiveToDefault(findAllVanitysInWorkspace(Constants.LIVE_WORKSPACE));
+}
+
+private void restoreWrongSystemName() throws RepositoryException {
+    recalculateVanitySystemName(findAllVanitysInWorkspace(Constants.EDIT_WORKSPACE));
+    recalculateVanitySystemName(findAllVanitysInWorkspace(Constants.LIVE_WORKSPACE));
+}
+
+private void recalculateVanitySystemName(List<JCRNodeWrapper> vanitys)  throws RepositoryException  {
+    JCRTemplate.getInstance().doExecuteWithSystemSessionAsUser(JahiaUserManagerService.getInstance().lookupRootUser().getJahiaUser(),
+            Constants.EDIT_WORKSPACE, null, session -> {
+        vanitys.forEach(vanity -> {
+            try {
+                JCRNodeWrapper currentVanity = session.getNodeByUUID(vanity.getIdentifier())
+                String newSystemName = JCRContentUtils.escapeLocalNodeName(currentVanity.getProperty("j:url").getString());
+                if (!currentVanity.getName().equals(newSystemName)) {
+                    log.info("recalculate system name launched for vanity url: " + currentVanity.getProperty("j:url").getString());
+                    log.info("new system name: " + newSystemName + " - (previous one was: " + currentVanity.getName() + ")");
+                    session.move(currentVanity.getPath(), currentVanity.getParent().getPath() + "/" + newSystemName);
+                }
+            } catch (RepositoryException e) {
+                log.error("Failed to recalculate the vanity url system name in workspace: ", e);
+            }
+        });
+        session.save();
+        return null;
+    });
 }
 
 private void copyMissingVanitysFromLiveToDefault(List<JCRNodeWrapper> vanitys) throws RepositoryException {
@@ -37,10 +64,10 @@ private void copyMissingVanitysFromLiveToDefault(List<JCRNodeWrapper> vanitys) t
     });
 }
 
-private static List<JCRNodeWrapper> findAllVanitysInLive() throws RepositoryException {
+private static List<JCRNodeWrapper> findAllVanitysInWorkspace(workspace) throws RepositoryException {
     return JCRTemplate.getInstance()
             .doExecuteWithSystemSessionAsUser(JahiaUserManagerService.getInstance().lookupRootUser().getJahiaUser(),
-                    Constants.LIVE_WORKSPACE, null, this::findAllVanitys);
+                    workspace, null, this::findAllVanitys);
 }
 
 private static List<JCRNodeWrapper> findAllVanitys(JCRSessionWrapper session) throws RepositoryException {
@@ -58,3 +85,4 @@ private static List<JCRNodeWrapper> findAllVanitys(JCRSessionWrapper session) th
 }
 
 restoreDeletedVanitys();
+restoreWrongSystemName();
