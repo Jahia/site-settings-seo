4949d60cd7c3517f433050b1dc7ca9accebf8f6e
sagnard <ksagnard@jahia.com>
2023-07-10 11:13:49 +0200

    BACKLOG-21272 : add test case url rewrite in live

---
+++ tests/cypress/e2e/urlrewrite.tests.cy.ts
@@ -0,0 +1,47 @@
+import { publishAndWaitJobEnding } from '@jahia/cypress'
+import { addVanityUrl } from '@jahia/cypress'
+
+describe('Basic test of vanity url access', () => {
+    const siteKey = 'digitall'
+    const sitePath = '/sites/' + siteKey
+    const homePath = sitePath + '/home'
+    const pageName = 'pageurlrewrite'
+    const pagePath = homePath + '/' + pageName
+
+    const createPage = (parent: string, name: string, template: string) => {
+        cy.apollo({
+            variables: {
+                parentPathOrId: parent,
+                name: name,
+                template: template,
+                language: 'en',
+            },
+            mutationFile: 'graphql/jcrAddPage.graphql',
+        })
+    }
+
+    before('create test data', function () {
+        cy.login()
+        createPage(homePath, pageName, 'home')
+        addVanityUrl(pagePath, 'en', '/page1seo1')
+        addVanityUrl(pagePath, 'en', '/page1-seo2')
+        addVanityUrl(pagePath, 'en', '/page1seo3')
+        publishAndWaitJobEnding(homePath)
+    })
+
+    after('clear test data', function () {
+        // deleteNode(pagePath)
+        publishAndWaitJobEnding(homePath)
+    })
+
+    it('Should redirect to the canonical vanity url', function () {
+        cy.visit(pagePath + '.html')
+        cy.url().should('include', 'page1seo3').should('not.include', '.html')
+
+        cy.visit(Cypress.env('JAHIA_URL') + '/page1seo1')
+        cy.url().should('include', 'page1seo3').should('not.include', 'page1seo1').should('not.include', '.html')
+
+        cy.visit(Cypress.env('JAHIA_URL') + '/page1-seo2')
+        cy.url().should('include', 'page1seo3').should('not.include', 'page1-seo2').should('not.include', '.html')
+    })
+})
