9e818c10f10652bf6eda4760d09cf813aeac6ee8
jsinovassin <jsinovassinnaik@jahia.com>
2023-09-05 16:13:21 +0200

    separate copy and cut test

--- tests/cypress/e2e/cutPaste.cy.ts
+++ tests/cypress/e2e/cutPaste.cy.ts
@@ -2,60 +2,22 @@ import { CustomPageComposer } from '../page-object/pageComposer/CustomPageCompos
 import { addVanityUrl, getVanityUrl, deleteNode, getNodeByPath } from '@jahia/cypress'
 import { addSimplePage } from '../utils/Utils'
 
-describe('Copy and Paste tests with Vanity Urls', () => {
+describe('Cut and Paste tests with Vanity Urls', () => {
     let pageToCopyPath
     before('Enable legacy page composer', () => {
         cy.apollo({ mutationFile: 'graphql/enableLegacyPageComposer.graphql' })
     })
 
-    beforeEach('Add page and vanity', () => {
+    it('Check for cut and paste', () => {
         cy.login()
         addSimplePage('/sites/digitall/home', 'to-copy-paste', 'To copy paste', 'en').then((page) => {
             pageToCopyPath = `/sites/digitall/home/${page.name}`
             addVanityUrl(pageToCopyPath, 'en', '/my-vanity')
         })
-        cy.logout()
-    })
-
-    it('Check for copy and paste (under same parent)', () => {
-        cy.login()
         const composer = new CustomPageComposer()
         CustomPageComposer.visit('digitall', 'en', 'home.html')
         let contextMenu = composer.openContextualMenuOnLeftTree('To copy paste')
-        contextMenu.copy()
-        contextMenu = composer.openContextualMenuOnLeftTree('Home')
-        contextMenu.paste()
-
-        cy.waitUntil(
-            () => {
-                return getNodeByPath('/sites/digitall/home/to-copy-paste-1').then((result) => {
-                    if (result?.data) {
-                        return result?.data
-                    }
-                    return false
-                })
-            },
-            {
-                errorMsg: 'Node not available in time',
-                timeout: 10000,
-                interval: 500,
-            },
-        ).then(() => {
-            getVanityUrl('/sites/digitall/home/to-copy-paste-1', ['en']).then((result) => {
-                expect(result?.data?.jcr?.nodeByPath?.vanityUrls).deep.eq([])
-            })
-        })
-
-        deleteNode('/sites/digitall/home/to-copy-paste-1')
-        cy.logout()
-    })
-
-    it('Check for copy and paste (under different parent)', () => {
-        cy.login()
-        const composer = new CustomPageComposer()
-        CustomPageComposer.visit('digitall', 'en', 'home.html')
-        let contextMenu = composer.openContextualMenuOnLeftTree('To copy paste')
-        contextMenu.copy()
+        contextMenu.cut()
         contextMenu = composer.openContextualMenuOnLeftTree('Newsroom')
         contextMenu.paste()
         cy.waitUntil(
@@ -79,9 +41,5 @@ describe('Copy and Paste tests with Vanity Urls', () => {
         })
         deleteNode('/sites/digitall/home/newsroom/to-copy-paste-1')
         cy.logout()
-    })
-
-    afterEach('Remove page', () => {
-        deleteNode(pageToCopyPath)
     })
 })
