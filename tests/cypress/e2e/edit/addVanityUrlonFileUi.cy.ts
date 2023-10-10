ea09d84bf72dda002f5dae34c9176c63cd695981
sagnard <ksagnard@jahia.com>
2023-10-10 14:29:19 +0200

    BACKLOG-21276 : migrate vanity URL on file test

---
+++ tests/cypress/e2e/edit/addVanityUrlonFileUi.cy.ts
@@ -0,0 +1,44 @@
+import { publishAndWaitJobEnding, editSite } from '@jahia/cypress'
+import { ContentEditorSEO } from '../../page-object/ContentEditorSEO'
+
+import { JContent } from '@jahia/jcontent-cypress/dist/page-object/jcontent'
+
+describe('Add or edit vanity Urls', () => {
+    const siteKey = 'digitall'
+    const sitePath = '/sites/' + siteKey
+    const fileName = 'foods.jpg'
+    const filePath = '/files/images/backgrounds/'
+    const jcontentFilePath = 'media' + filePath
+
+    before('set server test data', function () {
+        editSite('digitall', { serverName: 'jahia' })
+    })
+
+    after('clear server test data', function () {
+        editSite('digitall', { serverName: 'localhost' })
+    })
+
+    it('Add a first basic vanity URL on a file', function () {
+        cy.login()
+
+        JContent.visit('digitall', 'en', jcontentFilePath)
+
+        new JContent().switchToListMode()
+        new JContent().editComponentByText(fileName)
+
+        const contentEditor = new ContentEditorSEO()
+
+        const vanityUrlUi = contentEditor.openVanityUrlUi()
+        vanityUrlUi.addVanityUrl('vanityFile1', true)
+
+        publishAndWaitJobEnding(sitePath + filePath + fileName)
+
+        cy.request(Cypress.env('JAHIA_URL') + '/vanityFile1').then((response) => {
+            // expect(response.body).to.have.length(500)
+            const mime = response.headers['content-type']
+            expect(mime).to.contains('image/jpeg')
+            const length = response.headers['content-length']
+            expect(length).to.eq('83342')
+        })
+    })
+})
