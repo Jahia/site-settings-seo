04a7d62de179794df0aef222faaf729adea4555c
sagnard <ksagnard@jahia.com>
2023-10-12 14:26:13 +0200

    add check at the beginning of test

--- tests/cypress/e2e/edit/addVanityUrlonFileUi.cy.ts
+++ tests/cypress/e2e/edit/addVanityUrlonFileUi.cy.ts
@@ -3,7 +3,7 @@ import { ContentEditorSEO } from '../../page-object/ContentEditorSEO'
 
 import { JContent } from '@jahia/jcontent-cypress/dist/page-object/jcontent'
 
-describe('Add or edit vanity Urls', () => {
+describe('Add or edit vanity Urls on a file', () => {
     const siteKey = 'digitall'
     const sitePath = '/sites/' + siteKey
     const fileName = 'foods.jpg'
@@ -23,6 +23,13 @@ describe('Add or edit vanity Urls', () => {
 
         JContent.visit('digitall', 'en', jcontentFilePath)
 
+        // vanity url is not created yet, a direct access should triggers a 404 error
+        cy.request({ method: 'GET', url: Cypress.env('JAHIA_URL') + '/vanityFile1', failOnStatusCode: false }).then(
+            (response) => {
+                expect(response.status).to.eq(404)
+            },
+        )
+
         new JContent().switchToListMode()
         new JContent().editComponentByText(fileName)
 
@@ -33,7 +40,9 @@ describe('Add or edit vanity Urls', () => {
 
         publishAndWaitJobEnding(sitePath + filePath + fileName)
 
+        // vanity url is created yet, a direct access allows to download the file
         cy.request(Cypress.env('JAHIA_URL') + '/vanityFile1').then((response) => {
+            expect(response.status).to.eq(200)
             const mime = response.headers['content-type']
             expect(mime).to.contains('image/jpeg')
             const length = response.headers['content-length']
