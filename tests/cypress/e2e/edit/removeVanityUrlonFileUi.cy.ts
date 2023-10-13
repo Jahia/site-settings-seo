c61a4bafb5b4f896b9c85acb2388e4d450ccbebd
sagnard <ksagnard@jahia.com>
2023-10-13 15:08:55 +0200

    fix eslint

--- tests/cypress/e2e/edit/removeVanityUrlonFileUi.cy.ts
+++ tests/cypress/e2e/edit/removeVanityUrlonFileUi.cy.ts
@@ -1,9 +1,8 @@
-import {publishAndWaitJobEnding, editSite, addVanityUrl} from '@jahia/cypress'
+import { publishAndWaitJobEnding, editSite, addVanityUrl } from '@jahia/cypress'
 import { ContentEditorSEO } from '../../page-object/ContentEditorSEO'
-
 import { JContent } from '@jahia/jcontent-cypress/dist/page-object/jcontent'
 
-describe('Add or edit vanity Urls on a file', () => {
+describe('Remove vanity Urls from a file', () => {
     const siteKey = 'digitall'
     const sitePath = '/sites/' + siteKey
     const fileName = 'foods.jpg'
@@ -21,7 +20,6 @@ describe('Add or edit vanity Urls on a file', () => {
     })
 
     it('Add a first basic vanity URL on a file', function () {
-
         // vanity url is created, a direct access allows to download the file
         cy.request(Cypress.env('JAHIA_URL') + '/vanityFileToRemove').then((response) => {
             expect(response.status).to.eq(200)
@@ -46,11 +44,21 @@ describe('Add or edit vanity Urls on a file', () => {
         publishAndWaitJobEnding(sitePath + filePath + fileName)
 
         // vanity url is deleted, a direct access should triggers a 404 error
-        cy.request({ method: 'GET', url: Cypress.env('JAHIA_URL') + '/vanityFileToRemove', failOnStatusCode: false }).then(
-            (response) => {
-                expect(response.status).to.eq(404)
-            },
-        )
+        cy.request({
+            method: 'GET',
+            url: Cypress.env('JAHIA_URL') + '/vanityFileToRemove',
+            failOnStatusCode: false,
+        }).then((response) => {
+            expect(response.status).to.eq(404)
+        })
 
+        // Check direct access to the file's url still works
+        cy.request(Cypress.env('JAHIA_URL') + '/files/live' + sitePath + filePath + fileName).then((response) => {
+            expect(response.status).to.eq(200)
+            const mime = response.headers['content-type']
+            expect(mime).to.contains('image/jpeg')
+            const length = response.headers['content-length']
+            expect(length).to.eq('83342')
+        })
     })
 })
