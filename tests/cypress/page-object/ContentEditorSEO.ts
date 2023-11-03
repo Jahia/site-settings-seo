b5e226da05a999fa2b7b66a5cc28458b059bfa5c
jahia-carp <carp@jahia.com>
2023-11-03 11:46:49 +0100

    BACKLOG-21626: Update check permission test

--- tests/cypress/page-object/ContentEditorSEO.ts
+++ tests/cypress/page-object/ContentEditorSEO.ts
@@ -11,4 +11,9 @@ export class ContentEditorSEO extends ContentEditor {
         cy.get('li[data-sel-role="vanityUrls"]').click()
         return new VanityUrlUi()
     }
+
+    checkVanityUrlAccessibility(enabled) {
+        cy.get('button[data-sel-role="3dotsMenuAction"]').click()
+        cy.get('li[data-sel-role="vanityUrls"]').should('have.attr', 'aria-disabled', enabled)
+    }
 }
