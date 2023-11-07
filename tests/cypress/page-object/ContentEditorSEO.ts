ee9b0b0e995e04caee801c8d17d43814a2106568
ksagnard <ksagnard@jahia.com>
2023-11-07 08:31:55 -0800

    QA-14836 : add cypress test (#179)
    * QA-14836 : add cypress test
    
    * QA-14836 : fix after coderabbitai
    
    * QA-14836 : fix after comment

--- tests/cypress/page-object/ContentEditorSEO.ts
+++ tests/cypress/page-object/ContentEditorSEO.ts
@@ -16,4 +16,9 @@ export class ContentEditorSEO extends ContentEditor {
         cy.get('button[data-sel-role="3dotsMenuAction"]').click()
         cy.get('li[data-sel-role="vanityUrls"]').should('have.attr', 'aria-disabled', enabled)
     }
+
+    checkVanityUrlVisibility(isVisible) {
+        cy.get('button[data-sel-role="3dotsMenuAction"]').click()
+        cy.get('li[data-sel-role="vanityUrls"]').should(isVisible ? 'be.visible' : 'not.be.visible')
+    }
 }
