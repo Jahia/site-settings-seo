df20e214f4b1027425bf0259f12e2d1f8982242b
jsinovassin <jsinovassinnaik@jahia.com>
2024-06-27 11:00:18 +0200

    QA-15110: disable vanity publication on not published pages

--- src/javascript/components/actions/PublishVanityAction.jsx
+++ src/javascript/components/actions/PublishVanityAction.jsx
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import * as PropTypes from 'prop-types';
 import {allNotPublishedAndMarkedForDeletion, atLeastOneNotPublished} from '../Utils/Utils';
+import {useApolloClient} from '@apollo/client';
+import {GetPublicationStatus} from '~/components/gqlQueries';
+
+const contentIsVisibleInLive = publicationInfo => {
+    return publicationInfo.existsInLive && publicationInfo.publicationStatus !== 'UNPUBLISHED';
+};
 
 export const PublishVanityAction = ({render: Render, actions, urlPairs, ...otherProps}) => {
     const onClick = e => {
         actions.publishAction.call(urlPairs, e);
     };
 
-    const shouldBeHidden = allNotPublishedAndMarkedForDeletion(urlPairs) && atLeastOneNotPublished(urlPairs);
+    const [isVisibleInLive, setIsVisibleInLive] = useState(false);
+    const client = useApolloClient();
+
+    useEffect(() => {
+        const fetchPublicationData = async () => {
+            const data = await Promise.all(
+                urlPairs.map(async urlPair => {
+                    const {data} = await client.query({
+                        query: GetPublicationStatus,
+                        variables: {path: urlPair.default.targetNode.path, language: urlPair.default.language}
+                    });
+                    return data.jcr.nodeByPath.aggregatedPublicationInfo;
+                })
+            );
+            console.log(data);
+            setIsVisibleInLive(data.every(contentIsVisibleInLive));
+        };
+
+        fetchPublicationData();
+    }, [client, urlPairs, urlPairs.length]);
+
+    const shouldBeVisible = !allNotPublishedAndMarkedForDeletion(urlPairs) && atLeastOneNotPublished(urlPairs) && isVisibleInLive;
     return (
         <>
             <Render
                 {...otherProps}
-                isVisible={!shouldBeHidden}
+                isVisible={shouldBeVisible}
                 onClick={onClick}/>
         </>
     );
