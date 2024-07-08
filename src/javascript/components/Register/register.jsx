106d0ceeba8a27eba911e66016018bda456230a6
jsinovassin <jsinovassinnaik@jahia.com>
2024-07-08 14:49:11 +0200

    QA-15110: fix cache issue on language files

--- src/javascript/components/Register/register.jsx
+++ src/javascript/components/Register/register.jsx
@@ -1,5 +1,9 @@
 import {registerActions} from './registerActions';
 import {registerRoutes} from './registerRoutes';
+import hashes from './localesHash!';
+
+window.jahia.localeFiles = window.jahia.localeFiles || {};
+window.jahia.localeFiles['site-settings-seo'] = hashes;
 
 export const register = () => {
     registerRoutes();
