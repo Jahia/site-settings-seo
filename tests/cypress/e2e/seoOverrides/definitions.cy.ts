9ad6313ab87ed043d5e0c3daa2735ed5b6dfef63
ksagnard <ksagnard@jahia.com>
2025-01-03 16:35:38 +0100

    QABACKLOG-1651 : add test for SEO keyword edition (#253)

--- tests/cypress/e2e/seoOverrides/definitions.cy.ts
+++ tests/cypress/e2e/seoOverrides/definitions.cy.ts
@@ -1,12 +1,42 @@
-import { createSite, deleteSite } from '@jahia/cypress'
+import { addNode, createSite, deleteSite, getNodeByPath } from '@jahia/cypress'
 import { JContent } from '@jahia/jcontent-cypress/dist/page-object/jcontent'
 import { Field } from '@jahia/jcontent-cypress/dist/page-object/fields/field'
 
 describe('New SEO field definition tests', () => {
     const siteKey = 'seoFieldsTest'
+    const pageName_addTest = 'pagetest_addTag'
+    const pageName_removeTest = 'pagetest_removeTag'
+
+    const createPageWithSEOKeyword = (pageName: string) => {
+        return addNode({
+            parentPathOrId: `/sites/${siteKey}/home`,
+            name: pageName,
+            primaryNodeType: 'jnt:page',
+            mixins: ['jmix:seoHtmlHead'],
+            properties: [
+                {
+                    name: 'j:templateName',
+                    value: 'simple',
+                },
+                {
+                    name: 'jcr:title',
+                    value: pageName,
+                    language: 'en',
+                },
+                {
+                    name: 'seoKeywords',
+                    values: ['test2en', 'test1en'],
+                    language: 'en',
+                },
+            ],
+            children: [],
+        })
+    }
 
     before(function () {
         createSite(siteKey)
+        createPageWithSEOKeyword(pageName_addTest)
+        createPageWithSEOKeyword(pageName_removeTest)
     })
 
     after(function () {
@@ -39,6 +69,12 @@ describe('New SEO field definition tests', () => {
         picker.select()
 
         ce.save()
+
+        getNodeByPath(`/sites/${siteKey}/home`, ['seoKeywords']).then(({ data }) => {
+            expect(data.jcr.nodeByPath.properties.length).to.eq(1)
+            expect(data.jcr.nodeByPath.properties[0].values.length).to.eq(1)
+            expect(data.jcr.nodeByPath.properties[0].values[0]).to.eq('tag')
+        })
     })
 
     it('should not have new SEO fields for other types', function () {
@@ -49,5 +85,44 @@ describe('New SEO field definition tests', () => {
         assertFieldNotExist('htmlHead_jcr:description')
         assertFieldNotExist('htmlHead_seoKeywords')
         assertFieldNotExist('htmlHead_openGraphImage')
+    })
+
+    it('should be possible to edit SEO fields : adding a new value', function () {
+        const ce = JContent.visit(siteKey, 'en', 'pages/home/' + pageName_addTest).editPage()
+
+        ce.openSection('seo')
+
+        const tagField = ce.getField(Field, 'htmlHead_seoKeywords')
+        tagField.get().find('#htmlHead_seoKeywords').type('newtag{enter}', { delay: 500 })
+        tagField
+            .should('exist')
+            .and('be.visible')
+            .and('contain', 'test1en')
+            .and('contain', 'test2en')
+            .and('contain', 'newtag')
+        ce.save()
+
+        getNodeByPath(`/sites/${siteKey}/home/${pageName_addTest}`, ['seoKeywords']).then(({ data }) => {
+            expect(data.jcr.nodeByPath.properties.length).to.eq(1)
+            expect(data.jcr.nodeByPath.properties[0].values.length).to.eq(3)
+            expect(data.jcr.nodeByPath.properties[0].values).to.contains('newtag')
+        })
+    })
+
+    it('should be possible to edit SEO fields : removing a value', function () {
+        const ce = JContent.visit(siteKey, 'en', 'pages/home/' + pageName_removeTest).editPage()
+
+        ce.openSection('seo')
+
+        const tagField = ce.getField(Field, 'htmlHead_seoKeywords')
+        tagField.get().find('#htmlHead_seoKeywords').find('span:contains("test1en")').siblings('svg').click()
+        tagField.should('exist').and('be.visible').and('contain', 'test2en')
+        ce.save()
+
+        getNodeByPath(`/sites/${siteKey}/home/${pageName_removeTest}`, ['seoKeywords']).then(({ data }) => {
+            expect(data.jcr.nodeByPath.properties.length).to.eq(1)
+            expect(data.jcr.nodeByPath.properties[0].values.length).to.eq(1)
+            expect(data.jcr.nodeByPath.properties[0].values[0]).to.eq('test2en')
+        })
     })
 })
