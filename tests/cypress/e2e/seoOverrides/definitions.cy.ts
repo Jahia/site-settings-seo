975f6646ef130542ecc21215fc268eeace5d2119
Geofrey Flores <gflores@jahia.com>
2025-01-10 14:31:29 -0500

    Move check for SEO section to jcontent

--- tests/cypress/e2e/seoOverrides/definitions.cy.ts
+++ tests/cypress/e2e/seoOverrides/definitions.cy.ts
@@ -48,9 +48,8 @@ describe('New SEO field definition tests', () => {
         })
     })
 
-    it('should not have SEO section and new SEO fields for other types', function () {
+    it('should not have new SEO fields for other types', function () {
         JContent.visit(siteKey, 'en', 'content-folders/contents').createContent('Rich text')
-        cy.get(`[data-sel-content-editor-fields-group="seo"]`).should('not.exist', { timeout: 10000 })
 
         const assertFieldNotExist = (contentType) => {
             cy.get(`[data-sel-content-editor-field="${contentType}"]`).should('not.exist', { timeout: 10000 })
