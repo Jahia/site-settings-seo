8ee0c54ee9d30a36f626cfa212d49de6d4928141
jsinovassin <jsinovassinnaik@jahia.com>
2025-10-21 16:50:00 +0200

    ci: use sed to replace credentials in provisioning file

--- tests/env.run.sh
+++ tests/env.run.sh
@@ -54,7 +54,8 @@ else
   curl ${MANIFEST} --output ./run-artifacts/curl-manifest
   MANIFEST="curl-manifest"
 fi
-
+sed -i -e "s/NEXUS_USERNAME/$(echo ${NEXUS_USERNAME} | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/g" ./run-artifacts/${MANIFEST}
+sed -i -e "s/NEXUS_PASSWORD/$(echo ${NEXUS_PASSWORD} | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/g" ./run-artifacts/${MANIFEST}
 
 if [[ -d provisioning/ ]]; then
   cd provisioning/ || exit 1
