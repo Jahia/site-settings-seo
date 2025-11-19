eb80704ddce9be7bbfdad2129728cf89101f7e97
Jonathan SINOVASSIN-NAIK <58434978+jsinovassin@users.noreply.github.com>
2025-11-19 09:31:11 +0100

    feat: upgrade jahia version and remove localesHash files (#338)

--- src/javascript/components/Register/register.jsx
+++ src/javascript/components/Register/register.jsx
@@ -1,10 +1,6 @@
 import {registerActions} from './registerActions';
 import {registerRoutes} from './registerRoutes';
-import hashes from './localesHash!';
 import {registerPickers} from '~/components/Register/registerPickers';
-
-window.jahia.localeFiles = window.jahia.localeFiles || {};
-window.jahia.localeFiles['site-settings-seo'] = hashes;
 
 export const register = () => {
     registerRoutes();
